7be59f458f6ff1160fd539831fea7d87116785f1
lyju777 <lyju777@hanmail.net>
2025-08-15 18:09:53 +0900

    default layout로 이전

--- app.vue
+++ app.vue
@@ -1,46 +1,9 @@
 <template>
   <NuxtLayout>
-    <MenuBar />
     <NuxtPage />
-    <TimerFinishedDialog
-      v-if="finishedTimer"
-      v-model="isDialogVisible"
-      :timer="finishedTimer"
-    />
   </NuxtLayout>
 </template>
 
-<script setup lang="ts">
-import MenuBar from "~/components/MenuBar.vue";
-import { useTimersStore } from "~/stores/timers";
-import TimerFinishedDialog from "~/components/TimerFinishedDialog.vue";
-
-const timersStore = useTimersStore();
-const { finishedTimerId, timers } = storeToRefs(timersStore);
-
-const isDialogVisible = computed({
-  get: () => finishedTimerId.value !== null,
-  set: (value) => {
-    if (!value) {
-      timersStore.clearFinishedTimer();
-    }
-  },
-});
-
-const finishedTimer = computed(() => {
-  if (finishedTimerId.value === null) return undefined;
-  return timers.value.find((t) => t.id === finishedTimerId.value);
-});
-
-const { startAlarm, stopAlarm } = useTimerSound();
-
-watch(finishedTimerId, (newId) => {
-  if (newId !== null) {
-    startAlarm();
-  } else {
-    stopAlarm();
-  }
-});
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
---
+++ layouts/default.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <div class="card sticky z-10 top-0 left-0 right-0">
+      <Menubar :model="items">
+        <template #item="{ item, props, hasSubmenu }">
+          <NuxtLink
+            v-if="item.route"
+            v-slot="{ href, navigate }"
+            :to="item.route"
+            custom
+          >
+            <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+              <span :class="item.icon" />
+              <span>{{ item.label }}</span>
+            </a>
+          </NuxtLink>
+          <a
+            v-else
+            v-ripple
+            :href="item.url"
+            :target="item.target"
+            v-bind="props.action"
+          >
+            <span :class="item.icon" />
+            <span>{{ item.label }}</span>
+            <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down" />
+          </a>
+        </template>
+        <template #end>
+          <Button
+            severity="success"
+            variant="text"
+            label="Login"
+            icon="pi pi-sign-in"
+          />
+        </template>
+      </Menubar>
+    </div>
+    <slot />
+    <TimerFinishedDialog
+      v-if="finishedTimer"
+      v-model="isDialogVisible"
+      :timer="finishedTimer"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useTimersStore } from "~/stores/timers";
+import TimerFinishedDialog from "~/components/TimerFinishedDialog.vue";
+import { useTimerSound } from "~/composables/useTimerSound";
+
+const timersStore = useTimersStore();
+const { finishedTimerId, timers } = storeToRefs(timersStore);
+
+const router = useRouter();
+
+const items = ref([
+  {
+    label: "Home",
+    icon: "pi pi-home",
+    command: () => {
+      router.push("/");
+    },
+  },
+  {
+    label: "TimeChart",
+    icon: "pi pi-calendar-clock",
+    command: () => {
+      router.push("/timeLine");
+    },
+  },
+]);
+
+const isDialogVisible = computed({
+  get: () => finishedTimerId.value !== null,
+  set: (value) => {
+    if (!value) {
+      timersStore.clearFinishedTimer();
+    }
+  },
+});
+
+const finishedTimer = computed(() => {
+  if (finishedTimerId.value === null) return undefined;
+  return timers.value.find((t) => t.id === finishedTimerId.value);
+});
+
+const { startAlarm, stopAlarm } = useTimerSound();
+
+watch(finishedTimerId, (newId) => {
+  if (newId !== null) {
+    startAlarm();
+  } else {
+    stopAlarm();
+  }
+});
+</script>
+
+<style scoped></style>
